b91c33529d8c966700c35ff3afebc9bec03d1491
kirishima198 <1544415942@qq.com>
2026-09-05 21:08:48 +0800

    upload club-web/src/components/Pagination.vue

---
+++ club-web/src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pagination-wrap">
+    <el-pagination
+      :current-page="current"
+      :page-size="size"
+      :page-sizes="[5, 10, 20, 50]"
+      :total="total"
+      layout="total, sizes, prev, pager, next, jumper"
+      background
+      @current-change="onCurrentChange"
+      @size-change="onSizeChange"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  total: { type: Number, default: 0 },
+  current: { type: Number, default: 1 },
+  size: { type: Number, default: 10 }
+})
+const emit = defineEmits(['change'])
+
+function onCurrentChange(page) {
+  emit('change', { page, size: null })
+}
+function onSizeChange(size) {
+  emit('change', { page: 1, size })
+}
+</script>
+
+<style scoped>
+.pagination-wrap {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 16px;
+}
+</style>
