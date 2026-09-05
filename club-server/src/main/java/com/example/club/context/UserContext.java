d05ab27b0fc280db745c4ba2b2e92086309b1aca
kirishima198 <1544415942@qq.com>
2026-09-05 21:07:13 +0800

    upload club-server/src/main/java/com/example/club/context/UserContext.java

---
+++ club-server/src/main/java/com/example/club/context/UserContext.java
@@ -0,0 +1,23 @@
+package com.example.club.context;
+
+public class UserContext {
+
+    private static final ThreadLocal<LoginUser> HOLDER = new ThreadLocal<>();
+
+    public static void set(LoginUser user) {
+        HOLDER.set(user);
+    }
+
+    public static LoginUser get() {
+        return HOLDER.get();
+    }
+
+    public static Long getUserId() {
+        LoginUser user = HOLDER.get();
+        return user == null ? null : user.getId();
+    }
+
+    public static void remove() {
+        HOLDER.remove();
+    }
+}
