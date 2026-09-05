89de8c9eb3980557cb3a2dfa45d791499458f4d0
kirishima198 <1544415942@qq.com>
2026-09-05 21:07:29 +0800

    upload club-server/src/main/java/com/example/club/dto/ChangePasswordDTO.java

---
+++ club-server/src/main/java/com/example/club/dto/ChangePasswordDTO.java
@@ -0,0 +1,17 @@
+package com.example.club.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class ChangePasswordDTO {
+
+    @NotBlank(message = "原密码不能为空")
+    private String oldPassword;
+
+    @NotBlank(message = "新密码不能为空")
+    @Size(min = 6, max = 20, message = "新密码长度为6-20个字符")
+    private String newPassword;
+}
